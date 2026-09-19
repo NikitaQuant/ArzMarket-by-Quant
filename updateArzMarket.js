c801b538baaa7ae60f8835a78a771b73580f36b5
Никита <nikitapovshedny@gmail.com>
2026-09-19 20:38:05 +0300

    Update updateArzMarket.js

--- updateArzMarket.js
+++ updateArzMarket.js
@@ -1,6 +1,6 @@
 {
   "updateurl": "https://raw.githubusercontent.com/NikitaQuant/ArzMarket-by-Quant/main/by_Quant_ArzMarket%5B3_56%5D.lua",
-  "latest": "3.56.120",
+  "latest": "3.56.119",
   "itemsUpdate": 1,
   "componentsManifest": "https://raw.githubusercontent.com/NikitaQuant/ArzMarket-by-Quant/main/components_manifest.json",
   "bundleVersion": 279,
