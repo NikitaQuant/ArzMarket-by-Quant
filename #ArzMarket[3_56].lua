60bc04a1e92c8ef6bb1864a377f8d8ccb8048c49
NikitaQuant <nikitapovshedny@gmail.com>
2026-09-20 11:21:55 +0300

    afaffa

--- #ArzMarket[3_56].lua
+++
@@ -1,6 +0,0 @@
-script_name("ArzMarket duplicate guard")
-script_description("Disables the legacy duplicate. Active script: by_Quant_ArzMarket[3_56_112].lua")
-
-function main()
-	print("[ArzMarket] Legacy duplicate disabled. Active script: by_Quant_ArzMarket[3_56_112].lua")
-end
