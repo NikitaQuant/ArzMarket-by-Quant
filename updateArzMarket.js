25339fc6393b9353672d4e7a944e54f8d1c21d88
Никита <nikitapovshedny@gmail.com>
2026-09-20 11:22:56 +0300

    Delete updateArzMarket.js

--- updateArzMarket.js
+++
@@ -1,9 +0,0 @@
-{
-  "updateurl": "https://raw.githubusercontent.com/NikitaQuant/ArzMarket-by-Quant/main/by_Quant_ArzMarket%5B3_56%5D.lua",
-  "latest": "3.56.121",
-  "itemsUpdate": 1,
-  "componentsManifest": "https://raw.githubusercontent.com/NikitaQuant/ArzMarket-by-Quant/main/components_manifest.json",
-  "bundleVersion": 279,
-  "size": 1980422,
-  "crc32": "E88825A5"
-}
